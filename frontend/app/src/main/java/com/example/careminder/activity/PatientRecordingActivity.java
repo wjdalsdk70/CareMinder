1dde34eeb8fe8763223af72691973c7a237672c8
Havilash Sivaratnam <havilash2005@gmail.com>
2023-11-05 15:23:05 +0100

    button styling, initalize activitys, update activite_patient_recording.xml

---
+++ frontend/app/src/main/java/com/example/careminder/activity/PatientRecordingActivity.java
@@ -0,0 +1,18 @@
+package com.example.careminder.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.careminder.R;
+import com.google.android.material.button.MaterialButton;
+
+public class PatientRecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_recording);
+    }
+}
---
+++ frontend/app/src/main/java/com/example/careminder/activity/PatientRecordingResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.careminder.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.careminder.R;
+
+public class PatientRecordingResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_recording_result);
+    }
+}
