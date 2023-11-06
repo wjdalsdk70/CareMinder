4bb9d4799ffd349362f3ed011b28920ecbe87c93
Havilash Sivaratnam <havilash2005@gmail.com>
2023-11-06 21:11:10 +0100

    update patient recording activity

--- frontend/app/src/main/java/com/example/careminder/MainActivity.java
+++ frontend/app/src/main/java/com/example/careminder/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.careminder;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.careminder.activity.PatientActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.example.careminder.R;
+import com.example.careminder.activity.PatientRecordingActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +23,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnStartButtonClick(View view) {
-        Intent intent = new Intent(this, PatientActivity.class);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        } else {
+            startPatientRecordingActivity();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted
+                } else {
+                    // Permission denied - show a dialog
+                    showPermissionDialog();
+                }
+                return;
+            }
+        }
+    }
+
+    private void startPatientRecordingActivity() {
+        Intent intent = new Intent(this, PatientRecordingActivity.class);
         startActivity(intent);
+    }
+
+    private void showPermissionDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Microphone Permission Needed")
+                .setMessage("This app needs the Microphone permission to record audio. Please grant the permission.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+                    }
+                })
+                .setCancelable(false)
+                .show();
     }
 }
---
+++ frontend/app/src/main/java/com/example/careminder/view/VisualizerView.java
@@ -0,0 +1,73 @@
+package com.example.careminder.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.core.content.ContextCompat;
+import com.example.careminder.R;
+
+
+public class VisualizerView extends View {
+    private byte[] bytes;
+    private Paint paint = new Paint();
+
+    final int numLines = 20;
+
+    public VisualizerView(Context context) {
+        super(context);
+        init();
+    }
+
+    public VisualizerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        bytes = null;
+
+        paint.setAntiAlias(true);
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.black));
+        paint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    public void updateVisualizer(byte[] bytes) {
+        this.bytes = bytes;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (bytes == null) {
+            return;
+        }
+
+        float width = getWidth();
+        float height = getHeight();
+        int bytesPerLine = bytes.length / numLines;
+
+        float strokeWidth = width / numLines - 5;
+        paint.setStrokeWidth(strokeWidth);
+
+        float centerY = height / 2;
+        for (int i = 0; i < numLines; i++) {
+            int start = i * bytesPerLine;
+            int end = start + bytesPerLine;
+            float avgAbsValue = 0;
+            for (int j = start; j < end; j++) {
+                avgAbsValue += Math.abs(bytes[j]);
+            }
+            avgAbsValue /= bytesPerLine;
+
+            float relX = i * (width - strokeWidth) / (numLines - 1) + strokeWidth/2;
+            float absY = avgAbsValue / 128.0f * (height - strokeWidth) / 2;
+            canvas.drawLine(relX, centerY - absY, relX, centerY + absY, paint);
+        }
+    }
+
+}
