435ba1a06f4b6a35bae1105ce0a02ade6c904518
Havilash Sivaratnam <havilash2005@gmail.com>
2023-11-09 13:36:56 +0100

    update patient recording

--- frontend/app/src/main/java/com/example/careminder/activity/PatientRecordingActivity.java
+++ frontend/app/src/main/java/com/example/careminder/activity/PatientRecordingActivity.java
@@ -9,7 +9,9 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,8 +28,62 @@ public class PatientRecordingActivity extends AppCompatActivity {
     private int minBufferSize;
     private SpeechRecognizer speechRecognizer;
     private TextView textView;
-
     private Handler handler = new Handler();
+
+    RecognitionListener recognitionListener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+
+        }
+
+        @Override
+        public void onError(int error) {
+            Log.e("SpeechRecognition", "Error code: " + error);
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            if (matches != null)
+                textView.setText(matches.get(0));
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            ArrayList<String> matches = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            if (matches != null) {
+                textView.setText(matches.get(0));
+                Log.d("SpeechRecognition", "Result: " + matches.get(0));
+            }
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+
+        }
+    };
+
+
     private Runnable updateVisualizer = new Runnable() {
         @Override
         public void run() {
@@ -49,29 +105,26 @@ public class PatientRecordingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_patient_recording);
 
         handleAudio();
-
-//        textView = findViewById(R.id.recognizedText);
-//        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-//        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-//            @Override
-//            public void onResults(Bundle results) {
-//                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-//                if (matches != null) textView.setText(matches.get(0));
-//            }
-//
-//            // Implement other methods as needed
-//        });
-//        speechRecognizer.startListening(new Intent());
     }
 
     protected void handleAudio() {
-        visualizerView = findViewById(R.id.visualizer);
-
-        minBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
-            return;
         }
+
+        // recognize
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        textView = findViewById(R.id.recognized_text);
+        speechRecognizer.setRecognitionListener(recognitionListener);
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizer.startListening(intent);
+
+
+        // visualize
+        visualizerView = findViewById(R.id.visualizer);
+        minBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
         audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize);
 
         audioRecord.startRecording();
@@ -92,11 +145,30 @@ public class PatientRecordingActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted
+                } else {
+                    Intent intent = new Intent(this, PatientRecordingActivity.class);
+                    startActivity(intent);
+                }
+                return;
+            }
+        }
+    }
+
     public void OnBackButtonClick(View view) {
         Intent intent = new Intent(this, PatientActivity.class);
         startActivity(intent);
     }
 
     public void OnCancelClick(View view) {
+        Intent intent = new Intent(this, PatientActivity.class);
+        startActivity(intent);
     }
 }
