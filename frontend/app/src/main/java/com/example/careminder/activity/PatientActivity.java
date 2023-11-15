583d59d64d44ed35d4bb6e6c06e1ce9526660ca4
Gregory Reiter <greg1166@outlook.com>
2023-11-15 15:37:40 +0100

    patient menu

--- frontend/app/src/main/java/com/example/careminder/activity/PatientActivity.java
+++ frontend/app/src/main/java/com/example/careminder/activity/PatientActivity.java
@@ -33,99 +33,146 @@ public class PatientActivity extends AppCompatActivity {
 
         patientDao = new PatientDao(this);
 
-        popupContainer = findViewById(R.id.popupContainer);
+//        popupContainer = findViewById(R.id.popupContainer);
 
-//        getPatients();
+        LinearLayout menu = findViewById(R.id.horizontal_scroll_menu);
 
-        LinearLayout linearLayout = findViewById(R.id.patient_detail);
-        int numViews = 7;
-        String[] textArray = {
+        String[] items = {
                 "Name",
                 "Doctor",
                 "Nurse",
                 "1st Visit",
                 "Examinations",
                 "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
+                "Examinations",
                 "Hospitalization"
         };
+        for (int i = 0; i < items.length; i++) {
+            String item = items[i];
 
-        String[] secondRowTextArray = {
-                "Second row text 1",
-                "Second row text 2",
-                "Second row text 3",
-                "Second row text 4",
-                "Second row text 5",
-                "Second row text 6",
-                "Second row text 7"
-        };
+            TextView textView = new TextView(PatientActivity.this);
+            textView.setText(item);
+            textView.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
 
-        for (int i = 0; i < numViews; i++) {
-            LinearLayout rowLayout = new LinearLayout(this);
-            rowLayout.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(300, 90);
+            int margin = 16;
+            params.setMargins(margin, 0, margin, 0);
+            textView.setLayoutParams(params);
+            textView.setBackgroundResource(R.drawable.rounded_box_light);
 
-            TextView textView1 = new TextView(this);
-            LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(
-                    dpToPx(155), dpToPx(46)
-            );
+            int padding = 20;
+            textView.setPadding(padding, padding, padding, padding);
 
-            layoutParams1.setMargins(dpToPx(10), dpToPx(5), dpToPx(10), dpToPx(5));
-            textView1.setLayoutParams(layoutParams1);
-
-            if (i == 0 || i == numViews - 1) {
-                textView1.setBackgroundResource(R.drawable.rounded_box_dark);
-            } else {
-                textView1.setBackgroundResource(R.drawable.rounded_box_light);
-            }
-
-            String text1 = textArray[i];
-            textView1.setText(text1);
-            textView1.setGravity(Gravity.CENTER);
-            textView1.setSingleLine(false);
-            textView1.setTextSize(20);
-
-            TextView textView2 = new TextView(this);
-            LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, dpToPx(46)
-            );
-
-            layoutParams2.setMargins(dpToPx(10), dpToPx(5), dpToPx(10), dpToPx(5));
-            textView2.setLayoutParams(layoutParams2);
-
-            textView2.setBackgroundResource(R.drawable.rounded_box_response);
-
-            String text2 = secondRowTextArray[i];
-            textView2.setText(text2);
-            textView2.setGravity(Gravity.CENTER);
-            textView2.setSingleLine(false);
-            textView2.setTextSize(20);
-
-            rowLayout.addView(textView1);
-            rowLayout.addView(textView2);
-
-            linearLayout.addView(rowLayout);
+            menu.addView(textView);
         }
 
-        linearLayout.setOnTouchListener(new OnSwipeTouchListener(this) {
-            @Override
-            public void onSwipeLeft() {
-                popupContainer.setVisibility(View.VISIBLE);
-                popupContainer.animate().translationX(0f);
-            }
 
-            @Override
-            public void onSwipeRight() {
-                popupContainer.animate()
-                        .translationX(popupContainer.getWidth())
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                popupContainer.setVisibility(View.GONE);
-                                popupContainer.setTranslationX(0f);
-                            }
-                        });
-            }
-        });
     }
+
+
+
+//        getPatients();
+
+//        LinearLayout linearLayout = findViewById(R.id.patient_detail);
+//        int numViews = 7;
+//        String[] textArray = {
+//                "Name",
+//                "Doctor",
+//                "Nurse",
+//                "1st Visit",
+//                "Examinations",
+//                "Examinations",
+//                "Hospitalization"
+//        };
+//
+//        String[] secondRowTextArray = {
+//                "Second row text 1",
+//                "Second row text 2",
+//                "Second row text 3",
+//                "Second row text 4",
+//                "Second row text 5",
+//                "Second row text 6",
+//                "Second row text 7"
+//        };
+//
+//        for (int i = 0; i < numViews; i++) {
+//            LinearLayout rowLayout = new LinearLayout(this);
+//            rowLayout.setOrientation(LinearLayout.VERTICAL);
+//
+//            TextView textView1 = new TextView(this);
+//            LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(
+//                    dpToPx(155), dpToPx(46)
+//            );
+//
+//            layoutParams1.setMargins(dpToPx(10), dpToPx(5), dpToPx(10), dpToPx(5));
+//            textView1.setLayoutParams(layoutParams1);
+//
+//            if (i == 0 || i == numViews - 1) {
+//                textView1.setBackgroundResource(R.drawable.rounded_box_dark);
+//            } else {
+//                textView1.setBackgroundResource(R.drawable.rounded_box_light);
+//            }
+//
+//            String text1 = textArray[i];
+//            textView1.setText(text1);
+//            textView1.setGravity(Gravity.CENTER);
+//            textView1.setSingleLine(false);
+//            textView1.setTextSize(20);
+//
+//            TextView textView2 = new TextView(this);
+//            LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(
+//                    LinearLayout.LayoutParams.MATCH_PARENT, dpToPx(46)
+//            );
+//
+//            layoutParams2.setMargins(dpToPx(10), dpToPx(5), dpToPx(10), dpToPx(5));
+//            textView2.setLayoutParams(layoutParams2);
+//
+//            textView2.setBackgroundResource(R.drawable.rounded_box_response);
+//
+//            String text2 = secondRowTextArray[i];
+//            textView2.setText(text2);
+//            textView2.setGravity(Gravity.CENTER);
+//            textView2.setSingleLine(false);
+//            textView2.setTextSize(20);
+//
+//            rowLayout.addView(textView1);
+//            rowLayout.addView(textView2);
+//
+//            linearLayout.addView(rowLayout);
+
+//        linearLayout.setOnTouchListener(new OnSwipeTouchListener(this) {
+//        @Override
+//        public void onSwipeLeft() {
+//            popupContainer.setVisibility(View.VISIBLE);
+//            popupContainer.animate().translationX(0f);
+//        }
+//
+//        @Override
+//        public void onSwipeRight() {
+//            popupContainer.animate()
+//                    .translationX(popupContainer.getWidth())
+//                    .withEndAction(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            popupContainer.setVisibility(View.GONE);
+//                            popupContainer.setTranslationX(0f);
+//                        }
+//                    });
+//        }
+//    });
+
 
     public void OnRequestButtonClick(View view) {
         Intent intent = new Intent(this, PatientRecordingActivity.class);
@@ -135,12 +182,6 @@ public class PatientActivity extends AppCompatActivity {
     public void OnQuestionButtonClick(View view) {
         Intent intent = new Intent(this, PatientRecordingActivity.class);
         startActivity(intent);
-    }
-
-
-    public int dpToPx(int dp) {
-        float scale = getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
     }
 
     private void getPatients() {
