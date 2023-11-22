51178520d5825f5559887f0333e287d49e05a41f
Havilash Sivaratnam <havilash2005@gmail.com>
2023-11-22 15:28:45 +0100

    add chat messages

--- backend/careminder/request/views.py
+++ backend/careminder/request/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from careminder.permissions import CustomDjangoModelPermissions
-from .models import Request
-from .serializers import RequestSerializer
+import request
+from .models import Request, ChatMessage
+from .serializers import ChatMessageSerializer, RequestSerializer
 
 
 class RequestListCreateView(generics.ListCreateAPIView):
@@ -14,3 +15,27 @@ class RequestRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [CustomDjangoModelPermissions]
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
+
+
+class ChatMessageListCreateView(generics.ListCreateAPIView):
+    permission_classes = [CustomDjangoModelPermissions]
+    queryset = ChatMessage.objects.all()
+    serializer_class = ChatMessageSerializer
+
+    def get_queryset(self):
+        request_id = self.kwargs["request_pk"]
+        return ChatMessage.objects.filter(request__id=request_id)
+
+    def perform_create(self, serializer):
+        request = Request.objects.get(pk=self.kwargs["request_pk"])
+        serializer.save(request=request)
+
+
+class ChatMessageRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [CustomDjangoModelPermissions]
+    queryset = ChatMessage.objects.all()
+    serializer_class = RequestSerializer
+
+    def get_queryset(self):
+        request_id = self.kwargs["request_pk"]
+        return ChatMessage.objects.filter(request__id=request_id)
