c25c59688260b34019905ed13dfea91f892bff4f
Havilash Sivaratnam <havilash2005@gmail.com>
2023-11-22 13:37:29 +0100

    initalize Nurse, Secretary Page

---
+++ frontend/web/src/pages/Nurse/Nurse.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Nurse() {
+  return <div>Nurse</div>;
+}
---
+++ frontend/web/src/pages/Secretary/Secretary.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Secretary() {
+  return <div>Secretary</div>;
+}
