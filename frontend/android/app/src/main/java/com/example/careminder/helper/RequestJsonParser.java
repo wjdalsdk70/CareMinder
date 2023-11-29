6a9727f117c76496aae4456c4db2639e10ae4634
Gregory Reiter <greg1166@outlook.com>
2023-11-29 14:56:03 +0100

    model request

--- frontend/android/app/src/main/java/com/example/careminder/helper/RequestJsonParser.java
+++ frontend/android/app/src/main/java/com/example/careminder/helper/RequestJsonParser.java
@@ -5,48 +5,35 @@ import com.example.careminder.model.Request;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class RequestJsonParser {
-
-    public static Request parseRequest(String json) throws JSONException {
-        JSONObject jsonObject = new JSONObject(json);
-        String text = jsonObject.getString("text");
-        Integer state = jsonObject.getInt("state");
-        String timeString = jsonObject.getString("time");
-        DateTimeFormatter formatter = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            formatter = DateTimeFormatter.ISO_DATE_TIME;
-        }
-        LocalDateTime time = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            time = LocalDateTime.parse(timeString, formatter);
-        }
-
-        return new Request(text, state, time);
-    }
-
     public static Request parsePostRequest(String json) throws JSONException {
         JSONObject jsonObject = new JSONObject(json);
         String text = jsonObject.getString("text");
-        Integer state = jsonObject.getInt("state");
-        String timeString = jsonObject.getString("time");
-        DateTimeFormatter formatter = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            formatter = DateTimeFormatter.ISO_DATE_TIME;
-        }
-        LocalDateTime time = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            time = LocalDateTime.parse(timeString, formatter);
-        }
-        int id = jsonObject.getInt("id");
-        Integer forRole = jsonObject.optInt("forRole"); // optInt handles null values
-        Integer forRoleId = jsonObject.optInt("forRoleId");
-        boolean isQuestion = jsonObject.optBoolean("isQuestion");
-        int tabletId = jsonObject.optInt("tabletId");
+        byte[] recording = jsonObject.get("recording").toString().getBytes();
+        boolean isQuestion = jsonObject.getBoolean("isQuestion");
+        Date time = Date.valueOf(jsonObject.getString("time"));
+        Integer tablet = jsonObject.getInt("tablet");
 
-        return new Request(id, text, forRole, forRoleId, isQuestion, state, time, tabletId);
+        return new Request(text, recording, isQuestion, time, tablet);
     }
+
+    public static Request parseGetRequest(String json) throws JSONException {
+        JSONObject jsonObject = new JSONObject(json);
+        String text = jsonObject.getString("text");
+        boolean isQuestion = jsonObject.getBoolean("isQuestion");
+        Request.State state = Request.State.valueOf(jsonObject.getString("state"));
+        Date time = Date.valueOf(jsonObject.getString("time")); //
+        String response = jsonObject.getString("response");
+        Date responseTime = Date.valueOf(jsonObject.getString("responseTime"));
+        Integer tablet = jsonObject.getInt("tablet");
+
+        return new Request(text, isQuestion, state, time, response, responseTime, tablet);
+    }
+
 }
 
