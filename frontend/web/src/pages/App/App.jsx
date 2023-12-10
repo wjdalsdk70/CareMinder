511e7ee93241de5e03663950993920d1f2dce227
Greg <greg1166@outlook.com>
2023-12-10 16:04:36 +0100

    added all pages

--- frontend/web/src/pages/App/App.jsx
+++ frontend/web/src/pages/App/App.jsx
@@ -1,9 +1,24 @@
 import { useLocation } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "src/pages/Nurse/Home/Home";
-import Admin from "src/pages/Nurse/Admin/Admin";
+
+
+
+import NurseHome from "src/pages/Nurse/Home/Home";
+import Settings from "src/pages/Nurse/Admin/Settings/Settings";
+import AddUser from "src/pages/Nurse/Admin/AddUser/AddUser";
+import EditUser from "src/pages/Nurse/Admin/EditUser/EditUser";
+import UserList from "src/pages/Nurse/Admin/UserList/UserList";
 import useSession from "src/hooks/useSession";
 import Login from "src/pages/Nurse/Login/Login";
+
+
+import PatientHome from "src/pages/Patient/Home/Home";
+import Agreement from "src/pages/Patient/Agreement/Agreement";
+import Recording from "src/pages/Patient/Recording/Recording";
+import RecordingResult from "src/pages/Patient/RecordingResult/RecordingResult";
+import Setup from "src/pages/Patient/Setup/Setup";
+
+
 import NurseHeader from "src/components/NurseHeader/NurseHeader";
 
 import "src/theme/global.css";
@@ -22,8 +37,18 @@ function App() {
           element={<Navigate to="/nurse/login?next=/nurse/home" />}
         />
         <Route path="/nurse/login" element={<Login session={session} />} />
-        <Route path="/nurse/home" element={<Home session={session} />} />
-        <Route path="/nurse/admin" element={<Admin session={session} />} />
+        <Route path="/nurse/home" element={<NurseHome session={session} />} />
+        <Route path="/nurse/admin/settings" element={<Settings session={session} />} />
+        <Route path="/nurse/admin/addUser" element={<AddUser session={session} />} />
+        <Route path="/nurse/admin/editUser" element={<EditUser session={session} />} />
+        <Route path="/nurse/admin/userList" element={<UserList session={session} />} />
+
+
+        <Route path="/patient/home" element={<PatientHome/>} />
+        <Route path="/patient/agreement" element={<Agreement/>} />
+        <Route path="/patient/recording" element={<Recording/>} />
+        <Route path="/patient/recordingResults" element={<RecordingResult/>} />
+        <Route path="/patient/setup" element={<Setup/>} />
       </Routes>
     </div>
   );
