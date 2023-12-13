0308a6de250466d11269361a23aa3d40bd174296
Havilash Sivaratnam <havilash2005@gmail.com>
2023-12-13 14:41:40 +0100

    update

---
+++ frontend/web/src/pages/Nurse/Home/Patients/Patients.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import Filter from "src/components/Filter/Filter";
+import { BiLoaderCircle } from "react-icons/bi";
+import { MdOutlineDownloading } from "react-icons/md";
+import Request from "src/components/Request/Request";
+import { getRequests as getRequests } from "src/lib/api";
+import NurseHeader from "src/components/NurseHeader/NurseHeader";
+
+import "./Patients.css";
+import "../Home.css";
+import { useRedirectToLogin } from "src/hooks/useSession";
+
+const Patients = ({ session }) => {
+  // useRedirectToLogin(session);
+  const [requests, setRequests] = useState([]);
+
+  async function load() {
+    try {
+      const resp = await getRequests();
+      setRequests(resp);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    load();
+    const timeoutId = setTimeout(load, 1000 * 5);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  const [selectedOptions, setSelectedOptions] = useState({});
+
+  const handleCheckboxChange = (event) => {
+    setSelectedOptions({
+      ...selectedOptions,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  return (
+    <>
+      <NurseHeader />
+      <main>
+        <div className="nurse__home-requests nurse__home">
+          <div className="container">
+            {/* add here */}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Patients;
