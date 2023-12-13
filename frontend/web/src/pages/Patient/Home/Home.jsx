a0d6525e4dba70f0ee6f274a9be3ee8564b31700
Gregory Reiter <greg1166@outlook.com>
2023-12-13 14:40:14 +0100

    patient commit

--- frontend/web/src/pages/Patient/Home/Home.jsx
+++ frontend/web/src/pages/Patient/Home/Home.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import "./Home.css";
+import Logo from "src/assets/logo.svg";
 
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <div className="patient__container">
+            <div className="Patient__menu">
+                <h1> Title</h1>
+                <h2> which hospital some text</h2>
+            </div>
+
+            <div className="patient__randq__container">
+                <div className="patient__request__container">
+                    <h1>Request</h1>
+                    <button>
+                        <img
+                            className="header__img"
+                            src={Logo}
+                            alt=""
+                            width="80px"
+                            height="80px"
+                        />
+                        <h1>tell us you request</h1>
+                        <h3>some text </h3>
+                    </button>
+                </div>
+
+                <div className="patient__question__container">
+                    <h1>Question</h1>
+                    <button>
+                        <img
+                            className="header__img"
+                            src={Logo}
+                            alt=""
+                            width="80px"
+                            height="80px"
+                        />
+                        <h1>tell us you question</h1>
+                        <h3>some text </h3>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    );
 }
