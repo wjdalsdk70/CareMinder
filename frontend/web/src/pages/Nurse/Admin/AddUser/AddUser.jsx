b9276d0bfcb0e8a5fc24ab5cf99bf8be733166a9
David Rodrigues <dro135638@stud.gibb.ch>
2023-12-14 11:27:23 +0100

    addUser Page is almost done, just missing the top left corner

--- frontend/web/src/pages/Nurse/Admin/AddUser/AddUser.jsx
+++ frontend/web/src/pages/Nurse/Admin/AddUser/AddUser.jsx
@@ -1,27 +1,53 @@
 import React from "react";
+import data from "src/data.json";
+import "./AddUser.css";
+import NurseHeader from "src/components/NurseHeader/NurseHeader";
+import { BsPersonFillAdd } from "react-icons/bs";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function AddUser() {
 
-    const inputs = [
-        { name: "Name" },
-        { name: "Company ID" },
-        { name: "ID" },
-        { name: "Password" },
-        { name: "NFC Data" }
-    ]
+    const nurse = data.nurse;
 
     return (
         <div>
-            <form>
-                <div>
-                    {inputs.map((input, index) => (
-                        <div key={index}>
-                            <p>{input.name}</p>
-                            <input placeholder={input.name}></input>
+            <NurseHeader />
+            <IoIosArrowBack />
+            <a>Back</a>
+            <BsPersonFillAdd size="10rem"/>
+            <h1>Add new User</h1>
+            <div id="data_form">
+                <form>
+                    <div className="input_field">
+                        <p>{nurse.userName}<span>{nurse.required}</span></p>
+                        <input placeholder={nurse.userName}></input>
+                    </div>
+                    <div className="input_field">
+                        <p>{nurse.companyID}</p>
+                        <input placeholder={nurse.companyID}></input>
+                    </div>
+                    <div className="input_field">
+                        <p>{nurse.ID}<span>{nurse.required}</span></p>
+                        <input placeholder={nurse.ID}></input>
+                    </div>
+                    <div className="input_field">
+                        <p>{nurse.password}<span>{nurse.required}</span></p>
+                        <input placeholder={nurse.password}></input>
+                    </div>
+                    <div className="input_field">
+                        <div className="input_field">
+                            <p>{nurse.nfcData}</p>
+                            <input placeholder={nurse.nfcData} style={{ width: "35vmin" }}></input>
                         </div>
-                    ))}
-                </div>
-            </form>
+                        <button className="change_data_button">Change Data</button>
+                    </div>
+
+                    <div id="bottom_buttons">
+                        <button className="cancel_button">Cancel</button>
+                        <button className="save_button" type="submit">Save</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
