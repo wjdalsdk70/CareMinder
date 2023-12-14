7b99f1986a32ec66b0a9801dd8fc1a7bb4530885
Nicolas Castillo Thöni <nicolas.castillothoeni@bwdbern.ch>
2023-12-14 21:54:10 +0100

    working on nurse home reqeust

--- frontend/web/src/components/PatientFooter/PatientFooter.jsx
+++ frontend/web/src/components/PatientFooter/PatientFooter.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaCircleInfo } from "react-icons/fa6";
 import "./PatientFooter.css";
-import data from "src/data.json";
 
 export default function Footer() {
   return (
@@ -9,7 +8,10 @@ export default function Footer() {
       <div className="footer__container">
         <FaCircleInfo size={48} className="info" />
         <h3>
-          {data.patient.notification}
+          Currently, there is a delay due to the influx of emergency patients.
+          We fully recognize the urgency of the patients, but due to the nature
+          of the emergency room, we are treating them according to their
+          priorities, so we ask for their understanding.{" "}
         </h3>
       </div>
     </footer>
