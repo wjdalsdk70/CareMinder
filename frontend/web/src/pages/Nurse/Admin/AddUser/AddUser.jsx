eabd5eb14d391089b5f3c7ec88968144fa15588a
David Rodrigues <dro135638@stud.gibb.ch>
2023-12-14 08:47:26 +0100

    AddUser started

--- frontend/web/src/pages/Nurse/Admin/AddUser/AddUser.jsx
+++ frontend/web/src/pages/Nurse/Admin/AddUser/AddUser.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function AddUser() {
-    return <div>AdminPageAddUser</div>;
+
+    const inputs = [
+        { name: "Name" },
+        { name: "Company ID" },
+        { name: "ID" },
+        { name: "Password" },
+        { name: "NFC Data" }
+    ]
+
+    return (
+        <div>
+            <form>
+                <div>
+                    {inputs.map((input, index) => (
+                        <div key={index}>
+                            <p>{input.name}</p>
+                            <input placeholder={input.name}></input>
+                        </div>
+                    ))}
+                </div>
+            </form>
+        </div>
+    );
 }
