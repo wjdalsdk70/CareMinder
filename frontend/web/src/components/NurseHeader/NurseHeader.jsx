23a04ca8af06d27fb2ca8832f5f6cea0e3cd7e79
Greg <greg1166@outlook.com>
2023-12-15 16:35:41 +0100

    added the stuff back to pages: nurse admin all, patient histtory, patient record

--- frontend/web/src/components/NurseHeader/NurseHeader.jsx
+++ frontend/web/src/components/NurseHeader/NurseHeader.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
 import Logo from "src/assets/logo.svg";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import data from "src/data.json";
 
 import "./NurseHeader.css";
+import {IoIosArrowBack} from "react-icons/io";
 
 export default function NurseHeader() {
-  return (
-    <header className="header">
-      <img className="header__img" src={Logo} alt="" />
-      <FaBars className="header__bars" />
-    </header>
-  );
+    return (
+        <header className="nurse-header">
+            <div className="header-nurse-container">
+                <img className="header-img" src={Logo} alt=""/>
+                <FaBars className="header-bars"/>
+            </div>
+
+            <div className="go_back">
+                <IoIosArrowBack size="32"/>
+                <h2>{data.nurse.nurseHeaderBack}</h2>
+            </div>
+        </header>
+    );
 }
