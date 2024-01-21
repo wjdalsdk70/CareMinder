307d582ebeb014414c1c9909ba50dae57d7a732c
blocho135597 <olo135597@stud.gibb.ch>
2024-01-21 20:44:41 +0100

    Recording Korea speech recognition

--- frontend/web/src/pages/Patient/Recording/Recording.jsx
+++ frontend/web/src/pages/Patient/Recording/Recording.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import data from "src/data.json";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from "react-icons/fa6";
-
 import { PiWaveformBold } from "react-icons/pi";
 import PatientFooter from "src/components/PatientFooter/PatientFooter";
 import Logo from "src/assets/logo.svg";
 import "./Recording.css";
+import SpeechRecognition ,{ useSpeechRecognition } from 'react-speech-recognition';
+
+import { LiveAudioVisualizer } from 'react-audio-visualize';
+
+
+
+
+
 export default function Recording() {
+    const navigate = useNavigate(); // Use useNavigate instead of useHistory
+    const { transcript, resetTranscript} = useSpeechRecognition();
+    
+   
+    useEffect(() => {
+
+        SpeechRecognition.startListening({ continuous: true, language: "ko" });
+        console.log("Listening starts");
+    
+    }, []);
+
+    const handleHomeClick = () => {
+        SpeechRecognition.stopListening();
+        navigate("/patient/home");
+    }
+
+    const handleFinishClick = () => {
+      
+        console.log("Finish recording");
+        SpeechRecognition.stopListening();
+        navigate("/patient/recordingresults", { state: { transcript: transcript } });
+      
+    }
+
     return (
         <div className="recording">
             <div className="header">
@@ -14,33 +45,26 @@ export default function Recording() {
                     <img
                         className="patient-img"
                         src={Logo}
+                        alt="Patient Logo"
                     />
-
                     <div className="home">
-                        <a href="/patient/home"><FaArrowLeft  size={50} color="black"/></a>
-
+                        <a href="/patient/home"><FaArrowLeft size={50} color="black" /></a>
                     </div>
-
                 </div>
-
-
             </div>
             <div className="body">
-                <PiWaveformBold size={200}/>
+                <PiWaveformBold size={200} />
             </div>
             <h2 className="record-text">Recording your voice</h2>
             <div className="record-buttons">
-                <button className="record-button">
+                <button className="record-button" onClick={handleHomeClick}>
                     Cancel
                 </button>
-                <a href="/patient/recordingresults">
-                    <button className="record-button">
-                        Finish
-                    </button>
-                </a>
+                <button className="record-button" onClick={handleFinishClick}>
+                    Finish
+                </button>
             </div>
-            <PatientFooter/>
+            <PatientFooter />
         </div>
-
-    )
+    );
 }
