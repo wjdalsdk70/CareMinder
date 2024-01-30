9ca72b2781f7c929ce3c4a253656c3144ed3d456
Havilash Sivaratnam <havilash2005@gmail.com>
2024-01-30 11:23:31 +0100

    remove comments

--- backend/careminder/patient/views.py
+++ backend/careminder/patient/views.py
@@ -5,12 +5,12 @@ from .serializers import PatientSerializer
 
 
 class PatientListCreateView(generics.ListCreateAPIView):
-    # permission_classes = [CustomDjangoModelPermissions]
+    permission_classes = [CustomDjangoModelPermissions]
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
 
 class PatientRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    # permission_classes = [CustomDjangoModelPermissions]
+    permission_classes = [CustomDjangoModelPermissions]
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
